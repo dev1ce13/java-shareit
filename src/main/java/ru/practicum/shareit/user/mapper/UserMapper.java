34f03ac27e7ce9c670035b287f1607895e06e459
Sergey <e9orkin.s@yandex.ru>
2022-10-08 01:08:45 +0300

    refactor: deleted excess comments

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -14,11 +14,6 @@ public class UserMapper {
     }
 
     public static User mapToUser(UserDto userDto) {
-        /*return User.builder()
-                .id(userDto.getId())
-                .name(userDto.getName())
-                .email(userDto.getEmail())
-                .build();*/
         return new User(
                 userDto.getId(),
                 userDto.getName(),
